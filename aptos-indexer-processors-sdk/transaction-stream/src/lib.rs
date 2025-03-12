e19bf6edcf5590430a0b4a267a3c5a9ece8212f7
Justin Chang <justin@aptoslabs.com>
2025-03-11 17:38:45 -0700

    reexport moree

--- aptos-indexer-processors-sdk/transaction-stream/src/lib.rs
+++ aptos-indexer-processors-sdk/transaction-stream/src/lib.rs
@@ -2,6 +2,6 @@ pub mod config;
 pub mod transaction_stream;
 pub mod utils;
 
-pub use aptos_transaction_filter::BooleanTransactionFilter;
+pub use aptos_transaction_filter::{filters::*, BooleanTransactionFilter};
 pub use config::TransactionStreamConfig;
 pub use transaction_stream::{TransactionStream, TransactionsPBResponse};
